dbcc6469e05e183063ddcf6b433479823c1244b6
Edgar Ochoa <eochoa3@ucol.mx>
2019-03-23 23:29:00 -0600

    Casos de prueba agregados

--- Ordenamiento.Pruebas/PruebasOrdenamiento.cs
+++ Ordenamiento.Pruebas/PruebasOrdenamiento.cs
@@ -3,11 +3,48 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Ordenamiento.Pruebas
 {
     [TestClass]
-    public class UnitTest1
+    public class PruebasOrdenamiento
     {
+        private int[] salidaEsperada = new int[]{1,2,3,4,5};
+        private Ordenamiento.Burbuja ordenar = new Ordenamiento.Burbuja();
         [TestMethod]
-        public void TestMethod1()
+        public void Ordenado()
         {
+            int[] entrada = new int[]{1,2,3,4,5};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        [TestMethod]
+        public void UnoDesordenado()
+        {
+            int[] entrada = new int[]{1,3,2,4,5};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        [TestMethod]
+        public void UltimoAlPrincipio()
+        {
+            int[] entrada = new int[]{5,1,2,3,4};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        [TestMethod]
+        public void PrimeroAlFinal()
+        {
+            int[] entrada = new int[]{2,3,4,5,1};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        [TestMethod]
+        public void Invertidos()
+        {
+            int[] entrada = new int[]{5,4,3,2,1};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
         }
     }
 }
