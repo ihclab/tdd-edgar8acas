d093495fb346710901cf2d3c8920b7ce03a6b44f
Edgar Ochoa <eochoa3@ucol.mx>
2019-03-24 00:04:36 -0600

    Desarrollo del método de ordenamiento

--- Ordenamiento/Burbuja.cs
+++ Ordenamiento/Burbuja.cs
@@ -5,7 +5,19 @@ namespace Ordenamiento
     public class Burbuja
     {
         public void Ordenar(int[] array) {
-            throw new NotImplementedException("Crear los casos de prueba");
+            for (int i = 0; i < array.Length -1; i++)
+            {
+                for (int j = 0; j < array.Length - i -1; j++)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        int aux = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = aux;
+                    }    
+                }
+                
+            }
         }
     }
 }
